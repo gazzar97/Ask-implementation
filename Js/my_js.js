9546a382e6ad4bbff8039affb6a3b1bdd6615e38
mahmoudgamalfcis55 <mahmoudgamalfcis55@gmail.com>
2018-06-12 00:54:08 +0200

    Third Commit

--- Js/my_js.js
+++ Js/my_js.js
@@ -19,4 +19,49 @@ document.getElementById('abc').style.display = "none";
 document.getElementById('inputID').style.display = "block";
     
 }
+function setImg (){
+    document.getElementById('imgSource').src="../Navbar_Pictures/arrow_white.png";    
+}
+function retImg(){
+    
+    document.getElementById('imgSource').src="../Navbar_Pictures/arrow_orange.png";
+    
+}
+
+
+
+function setImg2 (){
+    document.getElementById('us2ImgSource').src="../Navbar_Pictures/arrow_white.png";    
+}
+function retImg2(){
+    
+    document.getElementById('us2ImgSource').src="../Navbar_Pictures/arrow_orange.png";
+    
+}
+
+
+
+function setImg3 (){
+    document.getElementById('us3ImgSource').src="../Navbar_Pictures/arrow_white.png";    
+}
+function retImg3(){
+    
+    document.getElementById('us3ImgSource').src="../Navbar_Pictures/arrow_orange.png";
+    
+}
+
+
+
+
+function setImg4(){
+    document.getElementById('us4ImgSource').src="../Navbar_Pictures/arrow_white.png";    
+}
+function retImg4(){
+    
+    document.getElementById('us4ImgSource').src="../Navbar_Pictures/arrow_orange.png";
+    
+}
+
+
+
 
